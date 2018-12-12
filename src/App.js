d267aeb029d9fdfe7ea14f37f750a9367093177b
Michael <michaeljosefcollins@gmail.com>
2018-12-12 13:07:44 +0100

    First stage

--- src/App.js
+++ src/App.js
@@ -1,28 +1,119 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from '@material-ui/core/Menu'
+
+import { Graph } from 'react-d3-graph';
+
+import './graph.css'
+import { MenuItem } from '@material-ui/core';
 
 class App extends Component {
+
+  data = {
+    nodes: [{id:"Standup", color : "#1587B8", size: 500}],
+    links: [{source: "Standup", target:"Standup"}]
+  }
+
+  constructor(){
+    super();
+    this.state = {
+      menu : {
+        show : false,
+        anchor : null
+      },
+      data : this.data
+    }
+  }
+
+  nodeRightClicked = (event, nodeId) => {
+    event.preventDefault()
+    this.setState({ menu: { show: true, anchor: event.target } })
+  };
+
+  handleClose = (event) => {
+    console.log(event.target)
+    this.setState({ menu: { show: false }})
+  }
+
+  addNode = (node, source) =>{
+    const data = this.data;
+    if(!data.nodes.some(el => el.id === node.id)) data.nodes.push(node)
+    if(!data.links.some(el => el.source === source && el.target === node.id)) data.links.push({ source: source, target: node.id })
+    this.setState(this.data)
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+       <Graph
+          id="graph-id" // id is mandatory, if no id is defined rd3g will throw an error
+          data={this.state.data}
+          config={myConfig}
+          onRightClickNode={this.nodeRightClicked}
+        />
+        <Menu
+          anchorEl={this.state.menu.anchor}
+          open={this.state.menu.show}
+          onClose={this.handleClose}>
+           {this.state.data.nodes.map(node => 
+            <MenuItem key={node.id} onClick={this.handleClose}>{node.id}</MenuItem>
+            )}
+            <MenuItem key="new" onClick={this.handleClose}>New...</MenuItem>
+        </Menu>
       </div>
     );
   }
 }
+
+
+// the graph configuration, you only need to pass down properties
+// that you want to override, otherwise default ones will be used
+const myConfig = {
+  nodeHighlightBehavior: true,
+  node: {
+      color: 'lightgreen',
+      size: 120,
+      highlightStrokeColor: 'blue'
+  },
+  link: {
+      highlightColor: 'lightblue'
+  }
+};
+
+
+
+// // graph event callbacks
+// const onClickGraph = function() {
+//   window.alert(`Clicked the graph background`);
+// };
+
+
+
+// const onRightClickNode = function(event, nodeId) {
+//   window.alert(`Right clicked node ${nodeId}`);
+// };
+
+// const onMouseOverNode = function(nodeId) {
+//   window.alert(`Mouse over node ${nodeId}`);
+// };
+
+// const onMouseOutNode = function(nodeId) {
+//   window.alert(`Mouse out node ${nodeId}`);
+// };
+
+// const onClickLink = function(source, target) {
+//   window.alert(`Clicked link between ${source} and ${target}`);
+// };
+
+// const onRightClickLink = function(event, source, target) {
+//   window.alert(`Right clicked link between ${source} and ${target}`);
+// };
+
+// const onMouseOverLink = function(source, target) {
+//   window.alert(`Mouse over in link between ${source} and ${target}`);
+// };
+
+// const onMouseOutLink = function(source, target) {
+//   window.alert(`Mouse out link between ${source} and ${target}`);
+// };
 
 export default App;
