c5fcd4813ad1e212e281e204ccc72126220649b0
xemc <0xemc@protonmail.com>
2025-09-19 06:09:46 +1000

    int

--- lang/run.ts
+++ lang/run.ts
@@ -2,31 +2,51 @@ import fs from "fs";
 import * as ohm from "ohm-js";
 
 // 1. Load the grammar file as text
-const grammarContents = fs.readFileSync("./transition.ohm", "utf-8");
+const grammarContents = fs.readFileSync("./transition.block.ohm", "utf-8");
 
 // 2. Compile the grammar
 const grammar = ohm.grammar(grammarContents);
 
 // 3. Create semantics to extract node names
+// const semantics = grammar.createSemantics().addOperation("nodes", {
+//   _iter(...children) {
+//     console.log(children.length);
+//     return children.map((child) => child.nodes());
+//   },
+//   lines(lines) {
+//     return lines.nodes();
+//   },
+//   line(rule, _eol) {
+//     return rule.nodes();
+//   },
+//   positions(from, arrow, to) {
+//     return [from.sourceString.trim(), to.sourceString.trim()];
+//   },
+//   comment(_, comment) {
+//     return comment.sourceString.trim();
+//   },
+//   step(tab, digit, dot, space, step) {
+//     return step.sourceString.trim();
+//   },
+// });
+
 const semantics = grammar.createSemantics().addOperation("nodes", {
   _iter(...children) {
-    console.log(children.length);
     return children.map((child) => child.nodes());
   },
-  Lines(lines) {
-    return lines.nodes();
+  Lines(Line, _eol, _, __eol) {
+    return [Line.sourceString];
   },
-  Line(rule) {
-    return rule.nodes();
-  },
-  Positions(from, _1, arrow, _2, to, _eol) {
-    return [from.sourceString.trim(), to.sourceString.trim()];
+  Line(line) {
+    return [line.sourceString];
   },
 });
 
 // 4. Read input from file and match against grammar
 const input = fs.readFileSync("./test.grpl", "utf-8");
 const matchResult = grammar.match(input);
+
+console.log(grammar.trace(input));
 
 if (matchResult.succeeded()) {
   const results = semantics(matchResult).nodes();
