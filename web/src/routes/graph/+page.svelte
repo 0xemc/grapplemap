c8e1b2ef18a97c3994c1f39f22a83954edaef94f
xemc <0xemc@protonmail.com>
2025-10-08 17:37:40 +1000

    custom nodes

--- web/src/routes/graph/+page.svelte
+++ web/src/routes/graph/+page.svelte
@@ -1,18 +1,23 @@
 <script>
 	import { Controls, MiniMap, SvelteFlow } from '@xyflow/svelte';
+	import TransitionNode from './TransitionNode.svelte';
 
-	import '@xyflow/svelte/dist/style.css';
-
+	const nodeTypes = { textUpdater: TransitionNode };
 	let nodes = $state.raw([
-		{ id: '1', position: { x: 0, y: 0 }, data: { label: '1' } },
+		{
+			id: 'node-1',
+			type: 'textUpdater',
+			position: { x: 0, y: 0 },
+			data: { text: 'some text' }
+		},
 		{ id: '2', position: { x: 0, y: 100 }, data: { label: '2' } }
 	]);
 
-	let edges = $state.raw([{ id: 'e1-2', source: '1', target: '2' }]);
+	let edges = $state.raw([{ id: 'e1-2', source: 'node-1', target: '2' }]);
 </script>
 
 <div style:width="100vw" style:height="100vh">
-	<SvelteFlow bind:nodes bind:edges fitView>
+	<SvelteFlow bind:nodes bind:edges {nodeTypes} fitView>
 		<MiniMap />
 		<Controls />
 	</SvelteFlow>
